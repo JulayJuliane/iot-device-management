ef4034634d691ebbd2cdf81e76563e754b85a67d
Juliane Roberta Conttroller <julianeroberta.conttroller@gmail.com>
2024-04-03 09:20:04 -0300

    passo 1

---
+++ src/app/device-details/device-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-device-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './device-details.component.html',
+  styleUrl: './device-details.component.css'
+})
+export class DeviceDetailsComponent {
+
+}
